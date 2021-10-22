de767b006c86a22edcf04858be688dd47b7b5c4a
mtkoroglu <m.tahakoroglu@gmail.com>
2021-10-22 22:10:07 +0300

    Add files via upload

--- project/web-cam-stream/web_cam_stream.py
+++ project/web-cam-stream/web_cam_stream.py
@@ -1,20 +1,14 @@
-import numpy as np
 import cv2
-
 cap = cv2.VideoCapture(0)
-
 while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
+    ret, frame = cap.read() # Capture frame-by-frame
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
+    (T, bw) = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite('bgr image.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
+        cv2.imwrite('gray scale image.jpg', gray, [cv2.IMWRITE_JPEG_QUALITY, 100])
+        cv2.imwrite('thresholded image.jpg', bw, [cv2.IMWRITE_JPEG_QUALITY, 100])
         break
-
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
