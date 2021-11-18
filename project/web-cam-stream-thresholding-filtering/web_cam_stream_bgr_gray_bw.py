e9f3f8011c60e49b63b0fc4e85303a6c2de3027a
mtkoroglu <m.tahakoroglu@gmail.com>
2021-11-18 04:06:37 +0300

    Add files via upload

--- project/web-cam-stream-thresholding-filtering/web_cam_stream_bgr_gray_bw.py
+++ project/web-cam-stream-thresholding-filtering/web_cam_stream_bgr_gray_bw.py
@@ -1,6 +1,6 @@
 import cv2
 cap = cv2.VideoCapture(0)
-if (cap.isOpened() == False):
+if (not cap.isOpened()):
     print('Web kamerasına erişimde sorun yaşandı!')
 else:
     print('Kameranın FPS değeri %i.' %cap.get(cv2.CAP_PROP_FPS))
@@ -12,13 +12,13 @@ while (cap.isOpened() == True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     (T, bw) = cv2.threshold(gray, 75, 255, cv2.THRESH_BINARY)
     if ret == True:
-        cv2.imshow('BGR image', frame)
-        cv2.imshow('Gray Scale image', gray)
-        cv2.imshow('Thresholded (Binary) image', bw)
+        cv2.imshow('renkli resim', frame)
+        cv2.imshow('gri tonlu resim', gray)
+        cv2.imshow('siyah beyaz resim', bw)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite('BGR webcam image.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
-            cv2.imwrite('gray scale webcam image.jpg', gray, [cv2.IMWRITE_JPEG_QUALITY, 100])
-            cv2.imwrite('thresholded webcam image.jpg', bw, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            cv2.imwrite('renkli resim.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            cv2.imwrite('gri tonlu resim.jpg', gray, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            cv2.imwrite('siyaz beyaz resim.jpg', bw, [cv2.IMWRITE_JPEG_QUALITY, 100])
             break
     else:
         print('Kare yakalanamadı!')
