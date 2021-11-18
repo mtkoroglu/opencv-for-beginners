a2a69257963379e0892c3f5399ef011deab36749
mtkoroglu <m.tahakoroglu@gmail.com>
2021-11-18 04:15:48 +0300

    Add files via upload

--- project/web-cam-stream-thresholding-filtering/web_cam_stream_bgr_gray_user_bw.py
+++ project/web-cam-stream-thresholding-filtering/web_cam_stream_bgr_gray_user_bw.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def gray_to_bw(img, T):
+def threshold(img, T):
     bwUser = np.zeros_like(img)
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
@@ -10,6 +10,7 @@ def gray_to_bw(img, T):
             else:
                 bwUser[i][j] = 255
     return bwUser
+
 
 cap = cv2.VideoCapture(0)
 if (not cap.isOpened()):
@@ -22,16 +23,16 @@ while (cap.isOpened() == True):
     dim = (int(s*frame.shape[1]), int(s*frame.shape[0]))
     frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #(T, bw) = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-    bw = gray_to_bw(gray, 60)
+    # (T, bw) = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+    bw = threshold(gray, 60)
     if ret == True:
-        cv2.imshow('BGR image', frame)
-        cv2.imshow('Gray Scale image', gray)
-        cv2.imshow('Thresholded (Binary) image', bw)
+        cv2.imshow('renkli resim', frame)
+        cv2.imshow('gri tonlu resim', gray)
+        cv2.imshow('siyah beyaz resim', bw)
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite('BGR webcam image.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
-            cv2.imwrite('gray scale webcam image.jpg', gray, [cv2.IMWRITE_JPEG_QUALITY, 100])
-            cv2.imwrite('thresholded webcam image.jpg', bw, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            cv2.imwrite('renkli resim.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            cv2.imwrite('gri tonlu resim.jpg', gray, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            cv2.imwrite('siyah beyaz resim.jpg', bw, [cv2.IMWRITE_JPEG_QUALITY, 100])
             break
     else:
         print('Kare yakalanamadı!')
