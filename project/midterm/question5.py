01b6fa9e8a4f8fa2174b420e0fceb4070f849085
mtkoroglu <m.tahakoroglu@gmail.com>
2021-12-01 23:57:42 +0300

    Add files via upload

--- project/midterm/question5.py
+++ project/midterm/question5.py
@@ -1,1 +1,8 @@
-
+import cv2
+picture = cv2.imread('abc.jpg')
+print(picture.shape[0])
+print(picture.shape[1])
+picture = cv2.resize(picture, (int(picture.shape[1]/2), int(picture.shape[0]/2)),
+interpolation = cv2.INTER_AREA)
+cv2.imshow('resim', picture)
+cv2.waitKey(0)
